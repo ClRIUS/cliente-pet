4824136f292d4788d6da237bf5ed205127bc6ff8
= <gabriel.cirilo.rosa@gmail.com>
2024-02-21 18:46:07 -0300

    implementa Cliente Repository, teste ok

--- src/main/java/br/com/petz/clientepet/cliente/application/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/infra/ClienteInfraRepository.java
@@ -2,11 +2,15 @@ package br.com.petz.clientepet.cliente.application.infra;
 
 import br.com.petz.clientepet.cliente.application.repository.ClienteRepositoy;
 import br.com.petz.clientepet.cliente.domain.Cliente;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
 @Repository
 @Log4j2
+@RequiredArgsConstructor
 public class ClienteInfraRepository implements ClienteRepositoy {
+    private final ClienteSpringDataJPARepository clienteSpringDataJPARepository;
+
     @Override
     public Cliente salva(Cliente cliente) {
         log.info("[inicia] ClienteInfraRepository - salva");
