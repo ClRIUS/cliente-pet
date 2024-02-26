e4a5155e79d67810f853eb49dd9b0f478ffd7ace
= <gabriel.cirilo.rosa@gmail.com>
2024-02-26 20:22:23 -0300

    Implementa fluxo repository, teste ok

--- src/main/java/br/com/petz/clientepet/cliente/application/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/infra/ClienteInfraRepository.java
@@ -5,6 +5,9 @@ import br.com.petz.clientepet.cliente.domain.Cliente;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 @Repository
 @Log4j2
 @RequiredArgsConstructor
@@ -17,5 +20,13 @@ public class ClienteInfraRepository implements ClienteRepositoy {
         clienteSpringDataJPARepository.save(cliente);
         log.info("[finaliza] ClienteInfraRepository - salva");
         return cliente;
+    }
+
+    @Override
+    public List<Cliente> buscaTodosClientes() {
+        log.info("[inicia] ClienteInfraRepository - buscaTodosClientes");
+        List<Cliente> todosClientes = clienteSpringDataJPARepository.findAll();
+        log.info("[finaliza] ClienteInfraRepository - buscaTodosClientes");
+        return todosClientes;
     }
 }
